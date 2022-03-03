43f631a3db0d85debb830dc5d6bb69fb1df6321d
sakib Malik <maliksakib347@gmail.com>
2022-03-03 19:00:30 +0530

    first

--- myshop/settings.py
+++ myshop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -129,3 +130,6 @@ SESSION_COOKIE_SECURE = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 CART_SESSION_ID = 'cart'
 CSRF_TRUSTED_ORIGINS = ["https://shopsakib.herokuapp.com"]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
