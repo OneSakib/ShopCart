449db4be7fb7d6776c68d25b4f09b7b5a82f879b
sakib Malik <maliksakib347@gmail.com>
2022-03-03 19:12:41 +0530

    deploy

--- myshop/settings.py
+++ myshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["shopsakib.herokuapp.com"]
+ALLOWED_HOSTS = ["shopsakib.herokuapp.com",'localhost']
 
 # Application definition
 
